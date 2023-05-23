20f4a480bd8b6a869dd496b658eda109f926f775
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-05-22 20:45:04 -0500

    Adding some verifications

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/Form.css';
 
 const LoginForm = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  const [isGoodPassword, setIsGoodPassword] = useState(true);
+  const [isGoodUserName, setIsGoodUserName] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,6 +14,31 @@ const LoginForm = () => {
     console.log('Password:', password);
   };
 
+  useEffect(() => {
+    if (password.length === 0) {
+      setIsGoodPassword(true);
+    }
+    if (password.length>0 && password.length >= 8) {
+      setIsGoodPassword(true);
+    }
+    if (password.length >0 && password.length < 8) {
+      setIsGoodPassword(false);
+    }
+  }, [password]);
+
+  // Do the same for userName
+  useEffect(() => {
+    if (userName.length === 0) {
+      setIsGoodUserName(true);
+    }
+    if (userName.length>0 && userName.length >= 5) {
+      setIsGoodUserName(true);
+    }
+    if (userName.length >0 && userName.length < 5) {
+      setIsGoodUserName(false);
+    }
+  }, [userName]);
+
   return (
     <form className="login" onSubmit={handleSubmit}>
     <div>
@@ -19,6 +46,14 @@ const LoginForm = () => {
     </div>
       <h2>Welcome, Panda!</h2>
       <p>It's time to fit in!</p>
+      {/* If it's good password set a div that has the class error password with the class active */}
+      <div className={`error-password ${!isGoodPassword ? 'on' : 'off'}`}>
+        Password must be at least 8 characters
+      </div>
+      {/* Do the same but with error username */}
+      <div className={`error-username ${!isGoodUserName ? 'on' : 'off'}`}>
+        Username must be at least 5 characters
+      </div>
       <input
         type="text"
         placeholder="User Name"
--- src/RegistrationForm.jsx
+++ src/RegistrationForm.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/Form.css';
 
 const RegistrationForm = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [isGoodPassword, setIsGoodPassword] = useState(true);
+  const [isGoodUserName, setIsGoodUserName] = useState(true);
+  const [isPasswordMatch, setIsPasswordMatch] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,6 +17,43 @@ const RegistrationForm = () => {
     console.log('Repeat Password:', repeatPassword);
   };
 
+  useEffect(() => {
+    if (password.length === 0) {
+      setIsGoodPassword(true);
+    }
+    if (password.length>0 && password.length >= 8) {
+      setIsGoodPassword(true);
+    }
+    if (password.length >0 && password.length < 8) {
+      setIsGoodPassword(false);
+    }
+  }, [password]);
+
+  // Do the same for userName
+  useEffect(() => {
+    if (userName.length === 0) {
+      setIsGoodUserName(true);
+    }
+    if (userName.length>0 && userName.length >= 5) {
+      setIsGoodUserName(true);
+    }
+    if (userName.length >0 && userName.length < 5) {
+      setIsGoodUserName(false);
+    }
+  }, [userName]);
+
+  useEffect(() => {
+    if (repeatPassword.length === 0) {
+      setIsPasswordMatch(true);
+    }
+    if (repeatPassword.length > 0 && repeatPassword === password) {
+      setIsPasswordMatch(true);
+    }
+    if (repeatPassword.length > 0 && repeatPassword !== password) {
+      setIsPasswordMatch(false);
+    }
+  }, [repeatPassword, password]);
+  
   return (
     <form className="login" onSubmit={handleSubmit}>
       <div>
@@ -21,6 +61,17 @@ const RegistrationForm = () => {
       </div>
       <h2>Create an Account</h2>
       <p>Join the Panda community today!</p>
+      {/* If it's good password set a div that has the class error password with the class active */}
+      <div className={`error-password ${!isGoodPassword ? 'on' : 'off'}`}>
+        Password must be at least 8 characters
+      </div>
+      {/* Do the same but with error username */}
+      <div className={`error-username ${!isGoodUserName ? 'on' : 'off'}`}>
+        Username must be at least 5 characters
+      </div>
+      <div className={`error-password-match ${!isPasswordMatch ? 'on' : 'off'}`}>
+        Passwords must match
+      </div>
       <input
         type="text"
         placeholder="User Name"
